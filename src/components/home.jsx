971f3cc45398a53fa312aa1cdabdf3568e3d6ed3
Christin-paige <christinpmartin@gmail.com>
2024-11-24 07:24:22 -0500

    added descending sorting to homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,17 +1,17 @@
-import {useState, useEffect} from 'react'
-import sanityClient from '../lib/sanityClient'
-import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import sanityClient from '../lib/sanityClient';
+import { useNavigate } from 'react-router-dom';
 //import imageUrlBuilder from '@sanity/image-url'
 
 // Set up the Sanity image URL builder
 //const builder = imageUrlBuilder(sanityClient)
 
 //function urlFor(source) {
-  //return builder.image(source)
+//return builder.image(source)
 //}
 
 export default function Home() {
-  const [galleryData, setGalleryData] = useState([])
+  const [galleryData, setGalleryData] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -26,18 +26,20 @@ export default function Home() {
         },
         alt,
         },
-    }`
+    }`;
 
     sanityClient
       .fetch(query)
       .then((data) => {
-        console.log(data) // Check if photos are in the data
-        setGalleryData(data)
+        // Sort galleries by order (ascending or descending)
+        const sortedData = data.sort((a, b) => a.order - b.order); // Change to b.order - a.order for descending
+        console.log(sortedData);
+        setGalleryData(sortedData);
       })
       .catch((error) => {
-        console.error('Sanity fetch error:', error)
-      })
-  }, [])
+        console.error('Sanity fetch error:', error);
+      });
+  }, []);
 
   const handleClick = (slug) => {
     navigate(`/${slug}`);
@@ -47,30 +49,26 @@ export default function Home() {
     <div>
       <div className="gallery-container">
         {galleryData.map((item, index) => (
-          <div key={index} 
-          className="galleryImage"
-          onClick={() => handleClick(item.slug.current)}
-          style={{ cursor: 'pointer' }}>
-           
-           <h2 className="galleryTitle">{item.title}</h2>
-           
-            {/* Ensure that item.photos is an object and not an array */}
+          <div
+            key={index}
+            className="galleryImage"
+            onClick={() => handleClick(item.slug.current)}
+            style={{ cursor: 'pointer' }}
+          >
+            <h2 className="galleryTitle">{item.title}</h2>
+
             {item.photos ? (
               <img
                 src={item.photos.asset.url}
                 alt={item.title}
                 style={{ maxWidth: '100%', height: 'auto' }}
-                
               />
-             
             ) : (
               <p>No photos available</p>
             )}
-            
-           
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
